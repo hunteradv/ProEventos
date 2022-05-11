3ffacf8db17c8759d4d2199b2adea610324a9207
HunterAdv <gustavopaulody@gmail.com>
2022-05-11 14:25:28 -0300

    inital commit

---
+++ Back/src/ProEventos.API/Controllers/EventController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProEventos.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EventController : ControllerBase
+    {
+        public EventController()
+        {
+            
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            return "value";
+        }
+    }
+}
