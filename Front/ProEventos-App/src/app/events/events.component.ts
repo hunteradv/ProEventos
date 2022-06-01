fb7e8819be2e543c5b24adab397d64eb006edb84
HunterAdv <gustavopaulody@gmail.com>
2022-06-01 16:58:24 -0300

    adicionando filtro de pesquisa

--- Front/ProEventos-App/src/app/events/events.component.ts
+++ Front/ProEventos-App/src/app/events/events.component.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-events',
@@ -9,11 +10,28 @@ import { Component, OnInit } from '@angular/core';
 export class EventsComponent implements OnInit {
 
   public events: any = []
-
+  public filteredEvents: any = []
   imgWidth: number = 150
   imgMargin: number = 2
   showImage: boolean = true
-  listFilter: string = ''
+  private _listFilter: string = ''
+
+  public get listFilter(): string {
+    return this._listFilter
+  }
+
+  public set listFilter(value: string) {
+    this._listFilter = value
+    this.filteredEvents = this.listFilter ? this.filterEvents(this.listFilter) : this.events
+  }
+
+  filterEvents(filterBy: string): any {
+    filterBy = filterBy.toLocaleLowerCase()
+    return this.events.filter(
+      (event: any) => event.title.toLocaleLowerCase().indexOf(filterBy) !== -1 ||
+                      event.local.toLocaleLowerCase().indexOf(filterBy) !== -1
+    )
+  }
 
   constructor(private http: HttpClient) { }
 
@@ -27,7 +45,10 @@ export class EventsComponent implements OnInit {
 
   public getEvents(): void {
     this.http.get('https://localhost:5001/api/events').subscribe(
-      response => this.events = response,
+      response => {
+        this.events = response
+        this.filteredEvents = this.events
+      },
       error => console.log(error)
     )
   }
